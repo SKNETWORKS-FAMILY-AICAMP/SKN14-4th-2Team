fe941f720ed9ae8b50018465818269eb9009649d
jmy0913 <87853586+jmy0913@users.noreply.github.com>
2025-08-08 18:56:19 +0900

    main.py 업데이트

--- jembot_all_docker/app/utils2/main.py
+++ jembot_all_docker/app/utils2/main.py
@@ -2,7 +2,7 @@ from .graph_setting import graph_setting
 
 compiled_graph = graph_setting()
 
-# 초급 전체 분기 실행 함수
+# 전체 분기 실행 함수 (기본 = 'basic')
 # level = 'basic' / 'intermediate' / 'advanced'
 def run_langraph(user_input, config_id, level='basic', chat_history=None):
     config = {"configurable": {"thread_id": config_id}}
